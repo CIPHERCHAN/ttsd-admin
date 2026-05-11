16b0264fb05f777691578e38bcfb4a6ab4709321
$T3CHS <136986527+CIPHERCHAN@users.noreply.github.com>
2026-05-12 01:34:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ttsd-admin-v5';
+const CACHE = 'ttsd-admin-v6';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install',  e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
